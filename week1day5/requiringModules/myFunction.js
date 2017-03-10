6db5f22037055e60c5a5f0c4ef0db90650801d68
Ellen Chang <imnunu@gmail.com>
2017-03-10 19:59:29 +0000

    Added

---
+++ week1day5/requiringModules/myFunction.js
@@ -0,0 +1,16 @@
+// var sqrtNumber = function (number) {
+//   var result = Math.sqrt(number);
+//   console.log(result);
+// }
+
+// module.exports = sqrtNumber;
+
+
+module.exports = {
+  name: 'sqrt calculator',
+  method: function (number) {
+        var result = Math.sqrt(number);
+        console.log(result);
+        }
+
+};
---
+++ week1day5/requiringModules/myRunningFile.js
@@ -0,0 +1,7 @@
+// var sqrtfunction = require('./myFunction.js');
+
+// sqrtfunction(144);
+
+var sqrtfunction = require('./myFunction.js');
+
+sqrtfunction.method(144);
